baec6823716b0b9691d4faaf23c40c47b3e851e7
John MacKintosh <johnmackintosh@users.noreply.github.com>
2024-01-02 20:30:15 +0000

    Update dot_I.R

--- dot_I.R
+++ dot_I.R
@@ -1,15 +1,18 @@
 library(data.table)
 
-DT <- fread("highdata.csv")
 
+#https://github.com/johnmackintosh/DT_dot_I
+
+
+DT <- fread("highdata.csv")
 lookup <- fread("https://raw.githubusercontent.com/johnmackintosh/ph_lookups/main/dz_intzone_cp_dz_names.csv")
 
 # names(DT)
 # names(lookup)
+
 # Data_Zone needs to join to "DataZone"
 # Could set key on both, but this interferes with existing row order
-
-# or specify by hand
+# so specify join "by hand"
 
 DT <- lookup[DT, on = .(DataZone = Data_Zone)]
 
@@ -64,7 +67,7 @@ DT[,
      max_health = .I[Health == max(Health)]),
    areaname]
 
-# view details for Inverness
+# view details for Inverness, with indices of 1 & 417
 DT[1, Health]  # lowest Health rank is 12
 DT[c(1,417), Health] # low / high is 12 / 6615
 
@@ -82,4 +85,44 @@ DT[DT[, .I[Health == max(Health)], by = .(areaname)]$V1]
 DT[DT[, .I[Health == max(Health)], by = .(areaname)]$V1
    ][,.SD, .SDcols = c("DataZone", "Health", 
                  "DataZone2011Name", "areaname")]
+
+setkey(DT, DataZone)
+# now the indices will change
+DT[,.I[Income == max(Income)]] # now 108, was 437
+
+# returns data.table
+DT[,.I[Health == max(Health)],.SD]  # now 95, was 424
+
+
+# https://stackoverflow.com/questions/73506180/select-rows-based-on-conditions-in-r
+id <- c(rep(102,9),rep(103,5),rep(104,4))
+status <- rep(c('single','relationship','relationship','single','single','single'),3)
+status <- factor(status, levels = c("single" ,"relationship"), ordered = TRUE)
+age <- c(17:19,22,23,26,28,32,33,21:25,21:24)
+DT <- data.table(id, status, age)
+
+newdata <- DT[c(1,2,7,8,13,14,18),]
+
+
+
+DT2 <- DT[DT[,.I[status == "single" & age == max(age) |  
+                   status == "relationship" & age == min(age)], 
+             .(rleid(status),id)]$V1
+          ][,rn := rleid(status),id
+            ][]
+
+#anti join
+
+#DT2[!DT2[,.I[(rn == 1 & status == "relationship")]]]
+
+DT2 <- DT2[!DT2[,.I[(rn == 1 & status == "relationship")]]
+           ][,rn := seq(.N), .(id, status)][]
+
+all.equal(DT2[,1:3], newdata)
+dcast(DT2,  
+      id ~   rn + status, 
+      value.var = "age")
+   
+
+
 
